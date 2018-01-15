0358d7a30f3e137565ff243b006ceecaa1fc8381
yumi <yumeishih17@gmail.com>
2018-01-15 18:03:29 +0800

    add basic scss structure

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -4,13 +4,9 @@ import React from "react";
 export default class Footer extends React.Component {
   render() {
     return (
-      <footer>
-        <div class="row">
-          <div class="col-lg-12">
-            <p>Copyright &copy; yumi</p>
-          </div>
-        </div>
-      </footer>
+    <footer>
+        <p>&copy; 2018 <strong>Yumi</strong></p>
+    </footer>
     );
   }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Layout from './components/Layout';
+import '../scss/style.scss';
 
 const app = document.getElementById('app');
 ReactDOM.render(<Layout />, app);
