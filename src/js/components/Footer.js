a0774b36e8ae34183d4cbcadb2dd034939ff3ef5
yumi <yumeishih17@gmail.com>
2018-01-16 13:19:50 +0800

    change a little bit style

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -4,9 +4,9 @@ import React from "react";
 export default class Footer extends React.Component {
   render() {
     return (
-    <footer>
-        <p>&copy; 2018 <strong>Yumi</strong></p>
-    </footer>
+        <footer>
+            <p>Copyright &copy; copy</p>
+        </footer>
     );
   }
 }
--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -4,10 +4,8 @@ export default class Header extends React.Component {
     render(){
         return(
             <header>
-                <div className = "container">
-                    <h1>Shopping Cart</h1>
-                    <span class="glyphicon glyphicon-shopping-cart"></span>
-                </div>
+                <h1>Shopping Cart</h1>
+                <span class="glyphicon glyphicon-shopping-cart"></span>
             </header>
         )
     }
