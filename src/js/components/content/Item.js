bb01be47d0889c94232420b9497e674cf9fa6cca
yumi <yumeishih17@gmail.com>
2018-01-17 15:51:32 +0800

    (css) item color

--- src/js/components/content/Item.js
+++ src/js/components/content/Item.js
@@ -5,9 +5,9 @@ export default class Item extends Component{
     render(){
         const item = this.props.item
         return (
-            <div class="item">
+            <div className="item">
                 <img src={item.itemImg} />
-                <div>
+                <div class="iteminfo">
                     <h4>{item.itemName}</h4>
                     <p>{item.describe}</p>
                     <p>Price: {item.price}</p>
