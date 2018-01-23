69a78737fd60c3efc40d49d9f3b513e23fddfb6b
yumi <yumeishih17@gmail.com>
2018-01-23 17:01:56 +0800

    update test env (jsdom)

---
+++ test/dom.js
@@ -0,0 +1,16 @@
+var jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+var exposedProperties = ['window', 'navigator', 'document'];
+
+const { document } = (new JSDOM('')).window;
+global.document = document;
+
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property);
+    global[property] = document.defaultView[property];
+  }
+});
+
+global.navigator = global.window.navigator;
